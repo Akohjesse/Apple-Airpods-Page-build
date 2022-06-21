5a642a95659a2db43fecc6637dcd6620189e4bcf
Jesse <akohgodwinjesse@gmail.com>
2022-06-21 20:33:20 +0100

    prettier

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
-import Header from './components/header.vue'
+import Header from "./components/header.vue";
 </script>
 
 <template>
-    <Header/>
+  <Header />
+  <div class="chapternav">
+    <div class="items">
+      <img src="../assets/airpods1.svg" alt="" />
+    </div>
+    <div class="items">
+      <img src="../assets/airpods3.svg" alt="" />
+    </div>
+    <div class="items">
+      <img src="../assets/airpodspro.svg" alt="" />
+    </div>
+    <div class="items">
+      <img src="../assets/airpodsmax.svg" alt="" />
+    </div>
+    <div class="items">
+      <img src="../assets/compare_light.svg" alt="" />
+    </div>
+    <div class="items">
+      <img src="../assets/apple_music_light.svg" alt="" />
+    </div>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
