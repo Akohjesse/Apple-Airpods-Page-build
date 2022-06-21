de6951cdca9ab53b1816815c8789aadeab27b6a9
Jesse <akohgodwinjesse@gmail.com>
2022-06-21 23:28:08 +0100

    3rd generation

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,63 @@
 <script setup>
 import Header from "./components/header.vue";
+import videoComp from "./components/airpods3.vue";
 </script>
 
 <template>
     <Header />
-    <div class="child_wrap">
-        <div class="chapternav">
-            <div class="items">
-                <img src="./assets/airpods2.svg" alt="" />
-                <span>AirPods <br> 2nd Generation</span>
-            </div>
-            <div class="items">
-                <img src="./assets/airpods3.svg" alt="" />
-                <span>AirPods <br> 3rd Generation <br></span>
-                <span class="new">New</span>
-            </div>
-            <div class="items">
-                <img src="./assets/airpodspro.svg" alt="" />
-                <span>AirPods Pro</span>
-            </div>
-            <div class="items">
-                <img src="./assets/airpodsmax.svg" alt="" />
-                <span>AirPods Max</span>
-            </div>
-            <div class="items">
-                <img src="./assets/compare_light.svg" alt="" />
-                <span>Compare</span>
-            </div>
-            <div class="items">
-                <img src="./assets/apple_music_light.svg" alt="" />
-                <span>Apple Music</span>
-            </div>
+    <div class="chapternav">
+        <div class="items">
+            <img src="./assets/airpods2.svg" alt="" />
+            <span
+                >AirPods <br />
+                2nd Generation</span
+            >
         </div>
+        <div class="items">
+            <img src="./assets/airpods3.svg" alt="" />
+            <span
+                >AirPods <br />
+                3rd Generation <br
+            /></span>
+            <span class="new">New</span>
+        </div>
+        <div class="items">
+            <img src="./assets/airpodspro.svg" alt="" />
+            <span>AirPods Pro</span>
+        </div>
+        <div class="items">
+            <img src="./assets/airpodsmax.svg" alt="" />
+            <span>AirPods Max</span>
+        </div>
+        <div class="items">
+            <img src="./assets/compare_light.svg" alt="" />
+            <span>Compare</span>
+        </div>
+        <div class="items">
+            <img src="./assets/apple_music_light.svg" alt="" />
+            <span>Apple Music</span>
+        </div>
+    </div>
+    <div class="child_wrap airpods-section">
+        <videoComp />
     </div>
 </template>
 
 <style lang="scss">
-.child_wrap {
-    width: 90%;
-    margin: 0 auto;
-    .chapternav {
-        text-align: center;
-        padding: 8px 0px;
-        @include flex(center, flex-start, 2.5rem);
-        .items{
-            span{
-                padding-top: .3rem;
-                color: $black-txt;
-                font-size: $font-sm;
-                display: block;
-            }
+.chapternav {
+    text-align: center;
+    padding: 8px 0px;
+    @include flex(center, flex-start, 2.5rem);
+    .items {
+        span {
+            padding-top: 0.3rem;
+            color: $black-txt;
+            font-size: $font-sm;
+            display: block;
         }
     }
+}
+.airpods-section {
+    padding: 2rem 0;
 }
 </style>
---
+++ src/components/airpods3.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="video_block">
+        <video
+            id="myVid"
+            loop
+            autoplay
+            src="https://www.apple.com/105/media/us/airpods/2021/625e09e3-e26e-49ed-a5ee-6e4eda5d275f/anim/supercut/large_2x.mp4"
+        ></video>
+        <div class="txt_content">
+            <div class="airpod-gen">
+                <div class="label-new">New</div>
+                <h1>AirPods</h1>
+                <p class="subhead">3rd generation</p>
+            </div>
+            <div class="purchase-gen">
+                <div class="label">Buy</div>
+                <p>Learn more</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "@vue/runtime-core";
+
+onMounted(() => {
+    window.addEventListener("mouseover", () => {
+        document.querySelector("#myVid").play();
+    });
+});
+</script>
+
+<style lang="scss">
+.video_block {
+    width: 100%;
+    position: relative;
+    video {
+        width: 100%;
+        border-radius: 2rem;
+    }
+    .txt_content {
+        position: absolute;
+        top: 35%;
+        left: 35%;
+        text-align: center;
+        @include flexcol(8rem 0);
+        .airpod-gen {
+            @include flexcol(1.5rem 0);
+            h1 {
+                @include txtheader($black-txt);
+            }
+        }
+        .purchase-gen {
+            .label {
+                padding: 0.8rem;
+                border-radius: 1.5rem;
+                width: 4.8rem;
+                background: $blue;
+                color: $white;
+            }
+            p {
+                color: $blue;
+            }
+        }
+    }
+}
+</style>
