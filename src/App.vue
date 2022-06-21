7000c338a4caa9d417f4b9a1823e72188a5c0fe4
Jesse <akohgodwinjesse@gmail.com>
2022-06-21 20:47:16 +0100

    fix route bug

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,38 @@ import Header from "./components/header.vue";
 </script>
 
 <template>
-  <Header />
-  <div class="chapternav">
-    <div class="items">
-      <img src="../assets/airpods1.svg" alt="" />
+    <Header />
+    <div class="child_wrap">
+        <div class="chapternav">
+            <div class="items">
+                <img src="./assets/airpods2.svg" alt="" />
+            </div>
+            <div class="items">
+                <img src="./assets/airpods3.svg" alt="" />
+            </div>
+            <div class="items">
+                <img src="./assets/airpodspro.svg" alt="" />
+            </div>
+            <div class="items">
+                <img src="./assets/airpodsmax.svg" alt="" />
+            </div>
+            <div class="items">
+                <img src="./assets/compare_light.svg" alt="" />
+            </div>
+            <div class="items">
+                <img src="./assets/apple_music_light.svg" alt="" />
+            </div>
+        </div>
     </div>
-    <div class="items">
-      <img src="../assets/airpods3.svg" alt="" />
-    </div>
-    <div class="items">
-      <img src="../assets/airpodspro.svg" alt="" />
-    </div>
-    <div class="items">
-      <img src="../assets/airpodsmax.svg" alt="" />
-    </div>
-    <div class="items">
-      <img src="../assets/compare_light.svg" alt="" />
-    </div>
-    <div class="items">
-      <img src="../assets/apple_music_light.svg" alt="" />
-    </div>
-  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.child_wrap {
+    width: 90%;
+    margin: 0 auto;
+    .chapternav {
+        text-align: center;
+        @include flex(center, center, 5rem);
+    }
+}
+</style>
